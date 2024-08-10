34f0ce52cc6b03d66dbe18b562ba38773190481f
estefanymoreira <estefanymoreira06@gmail.com>
2024-08-10 09:19:43 -0300

    creado mostrar cartel

---
+++ evento.js
@@ -0,0 +1,10 @@
+const index = document.quertySelector(".index");
+const btn = document.quertySelector("button");
+function mostrarAlerta(){
+    alert('Hola!!')
+}
+
+/*btn.addEventListener("onclick", function() => {
+   
+     )
+}*/
