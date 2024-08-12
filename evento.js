13e688469cd2d9a2b9622fdf1943e9835493ea91
estefanymoreira <estefanymoreira06@gmail.com>
2024-08-12 00:23:54 -0300

    agregado controlador de evento en js

--- evento.js
+++ evento.js
@@ -1,10 +1,25 @@
-const index = document.quertySelector(".index");
-const btn = document.quertySelector("button");
-function mostrarAlerta(){
-    alert('Hola!!')
-}
+/*const index = document.quertySelector(".index");*/
 
-/*btn.addEventListener("onclick", function() => {
-   
-     )
+
+const div = document.quertySelector("div");
+function alertaDiv(){
+    alert('Hola! Soy el div');
+}
+div.addEventListener("onclick", alertaDiv);
+
+
+/*const btn = document.quertySelector("button");
+function mostrarAlerta(){ 
+    alert('Hola!!');
+}
+button.addEventListener("onclick", mostrarAlerta) => {
+    event.stopPropagation();
 }*/
+const button = document.querySelector("button");
+
+button.addEventListener("click", mostrarAlerta);
+
+function mostrarAlerta() {
+    alert("¡Hiciste clic en el botón!");
+    event.stopPropagation()
+}
